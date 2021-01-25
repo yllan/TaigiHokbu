75654d7eb43fcc73d69277fe18792e0c1216c16a
Yung-Luen Lan <yllan@me.com>
2021-01-26 00:36:59 +0800

    First commit

--- TaigiHokbu/AppDelegate.swift
+++ TaigiHokbu/AppDelegate.swift
@@ -14,13 +14,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.servicesProvider = self
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @objc func liamTaigi(_ pboard: NSPasteboard, userData: String, error: NSErrorPointer) {
+        // get the string
+        if let taigi = pboard.string(forType: .string) {
+            TaigiHokbu.parseTaigi(taigi) { (result) in
+                switch result {
+                case .success(let syntax):
+                    print("success: ", syntax)
+                    TaigiHokbu.liamTaigi(syntax.KIP)
+                case .failure(let error):
+                    print("failure:", error)
+                    break
+                }
+            }
+        }
+    }
 }
 
---
+++ TaigiHokbu/TaigiHokbu.swift
@@ -0,0 +1,68 @@
+//
+//  TaigiHokbu.swift
+//  TaigiHokbu
+//
+//  Created by Yung-Luen Lan on 2021/1/25.
+//
+
+import Cocoa
+import AVFoundation
+
+struct SyntaxStructure : Decodable {
+    var 漢字: String
+    var KIP: String
+    var 分詞: String
+}
+
+enum TaigiError : Error {
+    case jsonFormatIncorrect
+    case cannotEscapeString
+}
+
+struct TaigiHokbu {
+    
+    static var player: AVAudioPlayer? = nil
+    
+    static func parseTaigi(_ taigi: String, callback: @escaping (Result<SyntaxStructure, Error>) -> Void) {
+        if let taigiEncoded = taigi.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+           let url = URL(string: "https://hokbu.ithuan.tw/tau") {
+            var req = URLRequest(url: url)
+            req.httpMethod = "POST"
+            req.httpBody = "taibun=\(taigiEncoded)".data(using: .utf8)
+            req.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.dataTask(with: req) { (data, response, error) in
+                let decoder = JSONDecoder()
+                if let error = error {
+                    callback(.failure(error))
+                } else if let data = data,
+                   let syntaxStructure = try? decoder.decode(SyntaxStructure.self, from: data) {
+                    callback(.success(syntaxStructure))
+                } else {
+                    callback(.failure(TaigiError.jsonFormatIncorrect))
+                }
+            }
+            task.resume()
+        } else {
+            callback(.failure(TaigiError.cannotEscapeString))
+        }
+    }
+    
+    static func liamTaigi(_ kip: String) {
+        if let kipEncoded = kip.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+           let url = URL(string: "https://hapsing.ithuan.tw/bangtsam?taibun=\(kipEncoded)") {
+            
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    TaigiHokbu.player = try? AVAudioPlayer(data: data)
+                    if let player = TaigiHokbu.player {
+                        player.prepareToPlay()
+                        player.volume = 1.0
+                        player.play()
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}

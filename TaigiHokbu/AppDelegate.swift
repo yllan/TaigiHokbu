91a553ffbd0af873244c4002dfdd1c96ceb84fe5
Yung-Luen Lan <yllan@me.com>
2021-01-25 22:54:09 +0800

    Initial Commit

---
+++ TaigiHokbu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TaigiHokbu
+//
+//  Created by Yung-Luen Lan on 2021/1/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TaigiHokbu/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TaigiHokbu
+//
+//  Created by Yung-Luen Lan on 2021/1/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
